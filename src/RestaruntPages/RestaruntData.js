d3c93d03b8f005b7135a15bc2a025b6d4cf51210
Madesh <madeshdex10@gmail.com>
2024-12-30 20:27:31 +0530

    add dataset

---
+++ src/RestaruntPages/RestaruntData.js
@@ -0,0 +1,83 @@
+
+export const RestaruntData = [
+
+    {
+        id:1,
+        img:'https://themes.pixelstrap.net/zomo/landing/frontend/assets/images/product/vp-3.png',
+        cusines:'Mediterranean',
+        FoodName:'Smoked Salmon Bagel',
+        Rating:'4.0',
+        Price:'$8',
+        catagories:'Burger'
+    },
+    {
+        id:2,
+        img:'https://themes.pixelstrap.net/zomo/landing/frontend/assets/images/product/vp-2.png',
+        cusines:'Italian',
+        FoodName:'Burger & French fried combo',
+        Rating:'4.0',
+        Price:'$34',
+        catagories:'Burger'
+    },
+    {
+        id:3,
+        img:'https://themes.pixelstrap.net/zomo/landing/frontend/assets/images/product/vp-1.png',
+        cusines:'Maxican, Italian',
+        FoodName:'Ultimate Loaded cheese Pizza',
+        Rating:'4.0',
+         Price:'$26'
+    },
+    {
+        id:4,
+        img:'https://themes.pixelstrap.net/zomo/landing/frontend/assets/images/product/vp-6.png',
+        cusines:'South indian',
+        FoodName:'asta salad with balsamic vinegar',
+        Rating:'4.0',
+        Price:'$9',
+        catagories:'Fries'
+    },
+    {
+        id:5,
+        img:'https://themes.pixelstrap.net/zomo/landing/frontend/assets/images/product/vp-5.png',
+        cusines:' Russian',
+        FoodName:'Pari Pari Fries',
+        Rating:'4.0',
+         Price:'$8'
+    },
+    {
+        id:6,
+        img:'https://themes.pixelstrap.net/zomo/landing/frontend/assets/images/product/vp-4.png',
+        cusines:'Scandinavian, Maxican',
+        FoodName:'Smoked Salmon Bagel',
+        Rating:'4.0',
+         Price:'$12',
+         catagories:'Fries'
+    },
+    {
+        id:7,
+        img:'https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-PR00000892.jpg?ver=60.43',
+        cusines:'South Indian',
+        FoodName:'Bucket for Two',
+        Rating:'4.1',
+        Price:'$15',
+        catagories:'Fries'
+    },
+    {
+        id:8,
+        img:'https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-PR00002162.jpg?ver=60.43',
+        cusines:'American ,Mexican',
+        FoodName:'Mingles Bucket Meal',
+        Rating:'4.3',
+        Price:'$55',
+        catagories:'Fries'
+    },
+    {
+        id:9,
+        img:'https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-PR00000892.jpg?ver=60.43',
+        cusines:'South Indian',
+        FoodName:'Bucket for Two',
+        Rating:'4.1',
+        Price:'$15',
+        catagories:'Fries'
+    }
+]
